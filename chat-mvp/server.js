ac3b794e173446a1d619dcac39e579f6203657f0
Noma <philliswadin@gmail.com>
2025-05-12 12:42:11 +0200

    handling connections and disconnections for socket.io

--- chat-mvp/server.js
+++ chat-mvp/server.js
@@ -6,4 +6,21 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static('public'));
+
+io.on('connection', socket => {
+    console.log('A user connected');
+
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+
+
+
  
